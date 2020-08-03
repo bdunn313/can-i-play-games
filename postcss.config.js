c48ce1f82369eadc4dc288f65fa7bc13973d81ab
Brad Dunn <brad@autobooks.co>
2020-08-03 17:58:23 -0400

    whitelist stuff

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,11 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 
   // Include the extractor from this package
   defaultExtractor: require("@dylanirlbeck/tailwind-ppx").extractor,
+
+  // Ensure some styles make it through
+  options: {
+    whitelist: ["bg-gray-300", "m-0"]
+  }
 });
 
 module.exports = {
